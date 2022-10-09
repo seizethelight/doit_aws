1763aeea9e9b4ddcf5b999f35055bf12d82f0fc1
yulim0-0 <yulim98@icloud.com>
2022-10-09 22:43:18 +0900

    t스토어 헤더 수

--- src/main/java/com/sist/web/StoreRestController.java
+++ src/main/java/com/sist/web/StoreRestController.java
@@ -26,81 +26,126 @@ public class StoreRestController {
 //	
 //	 @Autowired
 //	    private BCryptPasswordEncoder encoder;//시큐리
-	 @GetMapping(value="store/list.do",produces = "text/plain;charset=utf-8")
-	    // text/html (Ajax) , text/xml (XML), text/plain (JSON)
-	    public String list(String type ,String page)
-	    {
-	 	   if(page==null)
-	 		   page="1";
-	 	   
-	 	   if(type==null)
-	 		   type="1";
-	 	  int cate=Integer.parseInt(type);
-	 	   String[] c_no={"","a","w","m"};
+	@GetMapping(value="store/list.do",produces = "text/plain;charset=utf-8")
+    // text/html (Ajax) , text/xml (XML), text/plain (JSON)
+    public String list(String type ,String page)
+    {
+ 	   if(page==null)
+ 		   page="1";
+ 	   
+ 	   if(type==null)
+ 		   type="1";
+ 	  int cate=Integer.parseInt(type);
+ 	   String[] c_no={"","a","w","m","k","n","i","g","f"};
 //System.out.println("list_type="+type);
-	 	   JSONArray arr=new JSONArray();//[] => {no,poster,title,curpage,totalpage},{no,poster,title}.....
-	 	   int k=0;
-	 	   int s=322;//man 시작번호 
-	 		  int curpage=Integer.parseInt(page);
-		 	   Map map=new HashMap();
-		 	   int rowSize=25;
-		 	   
-		 	   if(cate==3)
-		 	   {
-		 		  int start=(rowSize*curpage+s)-(rowSize-1);
-			 	   int end=(rowSize*curpage)+s;
-			 	   map.put("start", start);
-			 	   map.put("end", end);
-			 	   map.put("c_no", c_no[cate]);
-		 	   }
-		 	   else
-		 	   {
-		 		  int start=(rowSize*curpage)-(rowSize-1);
-			 	   int end=rowSize*curpage;
-			 	   map.put("start", start);
-			 	   map.put("end", end);
-			 	   map.put("c_no", c_no[cate]);
-		 	   }
-		 	  
-		 	   
-		 	   List<StoreVO> list=new ArrayList<StoreVO>();
-		 	   int totalpage=0;
-		 			   
-		 			  if(cate==1)//전체 
-		 			  {
-		 				  list=dao.storeaListData(map);
-		 				 totalpage=dao.storeaTotalPage(map);
-		 			  }
-		 			  else //여자,남자 (cate=2,3) 
-		 			  {
-		 				  list=dao.storeListData(map);
-		 				 totalpage=dao.storeTotalPage(map);
-		 			  }
-		 			 
-
-		 	   for(StoreVO vo:list)
-		 	   {
-		 		   JSONObject obj=new JSONObject();
-		 		   obj.put("s_no", vo.getS_no());
-		 		   obj.put("name", vo.getName());
-		 		   obj.put("poster", "http://"+vo.getPoster());
-		 		   obj.put("price", vo.getPrice());
-		 		   obj.put("first_price", vo.getFirst_price());
-
-		 		   if(k==0)
-		 		   {
-		 			   obj.put("curpage", curpage);
-		 	 		   obj.put("totalpage", totalpage);
-		 	 		   obj.put("type", type);
-		 	 		   
-		 		   }
-		 		   
-		 		   arr.add(obj);
-		 		   k++;
-		 	   }
-	 	  return arr.toJSONString();
+ 	   JSONArray arr=new JSONArray();//[] => {no,poster,title,curpage,totalpage},{no,poster,title}.....
+ 	   int k=0;
+ 	   int s=235;//man 시작번호 
+ 	   int p=409;//kids
+ 	   int g=623;//new
+ 	   int f=1002;//item
+ 	   int c=790;//g
+ 	   
+ 		  int curpage=Integer.parseInt(page);
+	 	   Map map=new HashMap();
+	 	   int rowSize=9;
+	 	   
+	 	   if(cate==3)
+	 	   {
+	 		  int start=(rowSize*curpage+s)-(rowSize-1);
+		 	   int end=(rowSize*curpage)+s;
+		 	   map.put("start", start);
+		 	   map.put("end", end);
+		 	   map.put("c_no", c_no[cate]);
+	 	   }
+	 	  else if(cate==4)//kids
+	 	   {
+	 		  int start=(rowSize*curpage+p)-(rowSize-1);
+		 	   int end=(rowSize*curpage)+p;
+		 	   map.put("start", start);
+		 	   map.put("end", end);
+		 	   map.put("c_no", c_no[cate]);
+	 	   }
+	 	   else if(cate==5)//new
+	 	   {
+	 		  int start=(rowSize*curpage+g)-(rowSize-1);
+		 	   int end=(rowSize*curpage)+g;
+		 	   map.put("start", start);
+		 	   map.put("end", end);
+		 	   map.put("c_no", c_no[cate]);
+	 	   }
+	 	  else if(cate==6)//items
+	 	   {
+	 		 int start=(rowSize*curpage+f)-(rowSize-1);
+		 	   int end=(rowSize*curpage)+f;
+		 	   map.put("start", start);
+		 	   map.put("end", end);
+		 	   map.put("c_no", c_no[cate]);
+	 	   }
+	 	  else if(cate==7)//golf
+	 	   {
+	 		 int start=(rowSize*curpage+c)-(rowSize-1);
+		 	   int end=(rowSize*curpage)+c;
+		 	   map.put("start", start);
+		 	   map.put("end", end);
+		 	   map.put("c_no", c_no[cate]);
+	 	   }
+	 	  else
+	 	   {
+	 		  int start=(rowSize*curpage)-(rowSize-1);
+		 	   int end=rowSize*curpage;
+		 	   map.put("start", start);
+		 	   map.put("end", end);
+		 	   map.put("c_no", c_no[cate]);
+	 	   }
 	 	  
-	 	 }
+	 	   
+	 	   List<StoreVO> list=new ArrayList<StoreVO>();
+	 	   int totalpage=0;
+	 			   
+	 			  if(cate==1)//전체 
+	 			  {
+	 				  list=dao.storeaListData(map);
+	 				 totalpage=dao.storeaTotalPage(map);
+	 			  }
+	 			  else //여자,남자 (cate=2,3) 
+	 			  {
+	 				  list=dao.storeListData(map);
+	 				 totalpage=dao.storeTotalPage(map);
+	 			  }
+	 			 
+
+	 	   for(StoreVO vo:list)
+	 	   {
+	 		   JSONObject obj=new JSONObject();
+	 		   obj.put("s_no", vo.getS_no());
+	 		 
+			   String name = vo.getName();
+			   if(name.length()>25) {
+				name = name.substring(0,25)+"...";
+				vo.setName(name);
+			   }
+					
+					 obj.put("name", vo.getName());
+			
+	 		   obj.put("poster", "http://"+vo.getPoster());
+	 		   obj.put("price", vo.getPrice());
+	 		   obj.put("first_price", vo.getFirst_price());
+
+	 		   if(k==0)
+	 		   {
+	 			   obj.put("curpage", curpage);
+	 	 		   obj.put("totalpage", totalpage);
+	 	 		   obj.put("type", type);
+	 	 		   
+	 		   }
+	 		   
+	 		   arr.add(obj);
+	 		   k++;
+	 	   }
+ 	  return arr.toJSONString();
+ 	  
+ 	 }
 	  @GetMapping(value="store/detail.do",produces = "text/plain;charset=utf-8")
 	    public String store_detail_vue(int s_no,int type)
 	    {
